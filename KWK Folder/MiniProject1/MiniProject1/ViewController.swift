227ca71c65511d5ad779d4b61d029236beb1332c
cheyennemowatt <88101665+cheyennemowatt@users.noreply.github.com>
2021-07-28 15:00:46 -0400

    3rd Commit

---
+++ KWK Folder/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Cheyenne Mowatt on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var fall: UILabel!
+    
+    @IBOutlet weak var smoothies: UILabel!
+    
+    @IBOutlet weak var pool: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+  
+    @IBAction func reveal(_ sender: UIButton) {
+    }
+    
+}
+
