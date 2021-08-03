30749223545a83126deeef03a5144203ef9c29a0
cheyennemowatt <88101665+cheyennemowatt@users.noreply.github.com>
2021-08-03 09:45:17 -0400

    Completed Iteration 1

---
+++ KWK Folder/ToDoList/ToDoList/CompletedToDoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CompletedToDoViewController.swift
+//  ToDoList
+//
+//  Created by Cheyenne Mowatt on 8/3/21.
+//
+
+import UIKit
+
+class CompletedToDoViewController: UIViewController {
+    var previousToDoTVC = ToDoTableViewController()
+    var selectedToDo = ToDoClass()
+    
+    @IBOutlet weak var toDoDisplay: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        toDoDisplay.text = selectedToDo.description
+
+    }
+    
+    @IBAction func completeTapped(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- KWK Folder/ToDoList/ToDoList/ToDoTableViewController.swift
+++ KWK Folder/ToDoList/ToDoList/ToDoTableViewController.swift
@@ -26,13 +26,14 @@ class ToDoTableViewController: UITableViewController {
         listOfToDo = createToDo()
     }
 
+
     // MARK: - Table view data source
 
   
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 2
+        return listOfToDo.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -62,6 +63,14 @@ class ToDoTableViewController: UITableViewController {
     // Override to support editing the table view.
     
     */
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // this gives us a single ToDo
+        let eachToDo = listOfToDo[indexPath.row]
+
+        performSegue(withIdentifier: "moveToCompletedToDoVC", sender: eachToDo)
+   }
+
 
     /*
     // Override to support rearranging the table view.
@@ -71,14 +80,19 @@ class ToDoTableViewController: UITableViewController {
     /*
     // Override to support conditional rearranging of the table view.    */
 
-    /*
+ 
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    //In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let nextAddToDoVC = segue.destination as? AddToDoViewController {
+                  nextAddToDoVC.previousToDoTVC = self
     }
-    */
-
+        if let nextCompletedToDoVC = segue.destination as? CompletedToDoViewController {
+                 if let choosenToDo = sender as? ToDoClass {
+                      nextCompletedToDoVC.selectedToDo = choosenToDo
+                      nextCompletedToDoVC.previousToDoTVC = self
+                 }
+            }
+}
 }
